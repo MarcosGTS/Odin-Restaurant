f0d345633a58dda9aed44bf39f283156014fb371
MarcosGTS <marcosgtsodre@gmail.com>
2023-02-23 17:38:45 -0300

    refactor section creation

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -19,22 +19,46 @@ function createLunchElement(lunch) {
   return lunchCard;
 }
 
+function createMenuSection(sectionName, sectionElements) {
+  const section = document.createElement('section');
+  section.classList.add('menu');
+
+  section.innerHTML = `
+    <h2 id="${sectionName}">${sectionName}</h2>
+    <ul id="${sectionName}-menu" class="menu-grid"> 
+    </ul>
+  `;
+
+  const sectionGrid = section.querySelector('.menu-grid');
+  sectionElements.forEach((lunch) => {
+    const newLunch = createLunchElement(lunch);
+    sectionGrid.appendChild(newLunch);
+  });
+
+  return section;
+}
+
 export default function createMenu() {
   const menu = document.createElement('div');
   menu.classList.add('menu-page');
 
-  menu.innerHTML = `
-    <section class="menu">
-      <h2 id="burguers">Burguers</h2>
-      <ul id="burguer-menu" class="menu-grid"> 
-      </ul>
-    </section>
-  `;
+  // menu.innerHTML = `
+  //   // <section class="menu">
+  //   //   <h2 id="burguers">Burguers</h2>
+  //   //   <ul id="burguer-menu" class="menu-grid">
+  //   //   </ul>
+  //   // </section>
+  // `;
 
-  const burguerList = menu.querySelector('#burguer-menu');
-  lunchs.forEach((lunch) => {
-    const lunchCard = createLunchElement(lunch);
-    burguerList.appendChild(lunchCard);
+  // const burguerList = menu.querySelector('#burguer-menu');
+  // lunchs.forEach((lunch) => {
+  //   const lunchCard = createLunchElement(lunch);
+  //   burguerList.appendChild(lunchCard);
+  // });
+
+  Object.keys(lunchs).forEach((dessertName) => {
+    const newSection = createMenuSection(dessertName, lunchs[dessertName]);
+    menu.appendChild(newSection);
   });
 
   return menu;
