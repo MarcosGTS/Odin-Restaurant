798d4dab5c130c7453b12f81c0ac8077e578213e
MarcosGTS <marcosgtsodre@gmail.com>
2023-02-23 17:39:29 -0300

    Create new food

--- src/menu/lunchs.js
+++ src/menu/lunchs.js
@@ -3,29 +3,84 @@ import burguer2 from './lunch_2.jpg';
 import burguer3 from './lunch_3.jpg';
 import burguer4 from './lunch_4.jpg';
 
-export default [
-  {
-    img: burguer1,
-    name: 'Burguer',
-    price: 10,
-    description: 'Smoked turkey, extra bacon, jalapeño Havarti, red onion, tomato, romaine, avocado, chipotle mayo, on Dutch crunch.',
-  },
-  {
-    img: burguer2,
-    name: 'Burguer',
-    price: 10,
-    description: 'A burguer',
-  },
-  {
-    img: burguer3,
-    name: 'Burguer',
-    price: 10,
-    description: 'A burguer',
-  },
-  {
-    img: burguer4,
-    name: 'Burguer',
-    price: 10,
-    description: 'A burguer',
-  },
-];
+import dessert1 from './dessert_1.jpg';
+import dessert2 from './dessert_2.jpg';
+
+import drink1 from './drink_1.jpg';
+import drink2 from './drink_2.jpg';
+import drink3 from './drink_3.jpg';
+import drink4 from './drink_4.jpg';
+
+export default {
+  burguer: [
+    {
+      img: burguer1,
+      name: 'Burguer',
+      price: 10,
+      description: 'Smoked turkey, extra bacon, jalapeño Havarti, red onion, tomato, romaine, avocado, chipotle mayo, on Dutch crunch.',
+    },
+    {
+      img: burguer2,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: burguer3,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: burguer4,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: burguer4,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    }],
+  drink: [
+    {
+      img: drink1,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: drink2,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: drink3,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: drink4,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+  ],
+  dessert: [
+    {
+      img: dessert1,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+    {
+      img: dessert2,
+      name: 'Burguer',
+      price: 10,
+      description: 'A burguer',
+    },
+  ],
+};
